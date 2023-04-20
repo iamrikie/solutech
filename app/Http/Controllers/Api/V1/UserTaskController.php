4fe851ed626823f6be175b27624276a610a83ffb
iamrikie <iamrikie@gmail.com>
2023-04-20 23:33:18 +0300

    Created the Controllers and implemented the logic inline with the erd and also documented the code for easy follow

--- app/Http/Controllers/Api/V1/UserTaskController.php
+++ app/Http/Controllers/Api/V1/UserTaskController.php
@@ -1,64 +1,121 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
+use App\Models\UserTask;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserTaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+    // Retrieve all usertasks from the database with the user, task, and status relationships
+    $userTasks = UserTask::with('user', 'task', 'status')->get();
+        
+    // Return a JSON response with the retrieved usertask and a 200 status code
+    return response()->json([
+        'success' => true,
+        'data' => $userTasks
+    ], 200);
+}
+
+    public function show(int $id): JsonResponse
+    {
+        // Retrieve the usertask with the specified ID from the database with eager loading
+        $userTask = UserTask::with('user', 'task', 'status')->find($id);
+
+         // If the usertask was not found, return a 404 status code and an error message
+        if (!$userTask) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User task not found'
+            ], 404);
+        }
+
+        // Return a JSON response with the retrieved usertask and a 200 status code
+        return response()->json([
+            'success' => true,
+            'data' => $userTask
+        ], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(Request $request): JsonResponse
     {
-        //
+        // Validate the request data
+        $validatedData = $request->validate([
+            'user_id' => ['required', 'integer'],
+            'task_id' => ['required', 'integer'],
+            'due_date' => ['required', 'date'],
+            'start_time' => ['required', 'date_format:H:i'],
+            'end_time' => ['required', 'date_format:H:i'],
+            'remarks' => ['nullable', 'string', 'max:100'],
+            'status_id' => ['required', 'integer']
+        ]);
+
+        // Create a new UserTask with the validated data
+        $userTask = UserTask::create($validatedData);
+
+        // Eager load the relationships and return a JSON response with the created UserTask and a 201 status code
+        $userTask->load(['user', 'task', 'status']);
+        return response()->json([
+            'success' => true,
+            'data' => $userTask
+        ], 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function update(Request $request, int $id): JsonResponse
     {
-        //
+        // Retrieve the UserTask with the specified ID from the database with eager loading
+        $userTask = UserTask::with(['user', 'task', 'status'])->find($id);
+
+         // If the UserTask was not found, return a 404 status code and an error message
+        if (!$userTask) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User task not found'
+            ], 404);
+        }
+
+        // Validate the request data
+        $validatedData = $request->validate([
+            'user_id' => ['required', 'integer'],
+            'task_id' => ['required', 'integer'],
+            'due_date' => ['required', 'date'],
+            'start_time' => ['required', 'date_format:H:i'],
+            'end_time' => ['required', 'date_format:H:i'],
+            'remarks' => ['nullable', 'string', 'max:100'],
+            'status_id' => ['required', 'integer']
+        ]);
+         // Update the UserTask with the validated data
+        $userTask->update($validatedData);
+
+        // Return a JSON response with the updated UserTask and a 200 status code
+        return response()->json([
+            'success' => true,
+            'data' => $userTask
+        ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function destroy(int $id): JsonResponse
     {
-        //
-    }
+         // Retrieve the UserTask with the specified ID from the database
+        $userTask = UserTask::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+        // If the UserTask was not found, return a 404 status code and an error message
+        if (!$userTask) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User task not found'
+            ], 404);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        $userTask->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'success' => true,
+            'message' => 'User task deleted successfully'
+        ], 204);
     }
 }
