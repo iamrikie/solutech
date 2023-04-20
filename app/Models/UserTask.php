9ff79580da88ba8614a022e2edfffcbbc2419cff
iamrikie <iamrikie@gmail.com>
2023-04-20 09:11:19 +0300

    Created User, Task, UserTask & Status Models

---
+++ app/Models/UserTask.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTask extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'task_id', 'due_date', 'start_time', 'end_time', 'remarks', 'status_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+}
