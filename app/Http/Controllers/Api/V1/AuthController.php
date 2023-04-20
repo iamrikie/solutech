92236e74babc7b51db1d4dc6659859679ed8a169
iamrikie <iamrikie@gmail.com>
2023-04-21 00:23:09 +0300

    Created AuthController and logic for handling authentication-related functionalities. Created authentication middleware to the routes, and Login / Logout routes

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -1,10 +1,44 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login successful'
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Invalid credentials'
+        ], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful'
+        ]);
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApI\V1\TaskController;
+use App\Http\Controllers\ApI\V1\UserController;
+use App\Http\Controllers\ApI\V1\StatusController;
+use App\Http\Controllers\ApI\V1\UserTaskController;
+use App\Http\Controllers\ApI\V1\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('/users', UserController::class);
+        Route::apiResource('/statuses', StatusController::class);
+        Route::apiResource('/tasks', TaskController::class);
+        Route::apiResource('/usertasks', UserTaskController::class);
+    });
+});
+
+        
